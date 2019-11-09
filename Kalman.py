c526311a1f7022a1d2295a26798f4bd9b234206b
Andrew Hepp <drewhepp@umich.edu>
2019-11-08 21:31:50 -0500

    4d kalman filter

--- Kalman.py
+++ Kalman.py
@@ -5,23 +5,66 @@ class Kalman:
         self.last_state = initial_state
         self.last_covar = initial_covar
 
-    def estimate(self, measured, acceleration, dt):
+    def estimate(self, measurement, acceleration, dt):
+        # Predict the state and covariance
         A = numpy.array([
             [1, dt, 0, 0 ], # x
             [0,  1, 0, 0 ], # x'
             [0,  0, 1, dt], # y
-            [0,  0, 0, 1 ]  #y'
+            [0,  0, 0, 1 ]  # y'
         ])
-
+        a = .5 * dt ** 2 # acceleration term
         B = numpy.array([
-            [.5 * dt ** 2, 0], #x''
-            [dt,           0], #x'
-            [0, .5 * dt ** 2], #y''
-            [0, dt          ]  #y'
+            [a,  0 ], #x''
+            [dt, 0 ], #x'
+            [0,  dt], #y''
+            [0,  a ]  #y'
         ])
+        predicted_state = numpy.dot(A, self.last_state) + numpy.dot(B, acceleration) # + noise
+        predicted_covar = numpy.dot(numpy.dot(A, self.last_covar), numpy.transpose(A)) # + noise
 
-        state = numpy.dot(A, self.last_state) + numpy.dot(B, acceleration) # + noise
+        # Mask the covariance, since x and y are known to be independent
+        predicted_covar = numpy.diag(numpy.diag(predicted_covar))
 
-        self.last_state = state
-        #self.last_covar = covar
-        return state
+
+        # Extract position to incorporate into our model
+        C = numpy.array([
+            [1, 0, 0, 0], # x
+            [0, 1, 0, 0], # x'
+            [0, 0, 0, 0], # y
+            [0, 0, 0, 0]  # y'
+        ])
+        observation = numpy.dot(C, measurement) # + noise
+
+        # Calculate Kalman gain to correct our prediction based on our measurement
+        I = numpy.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ])
+        R = numpy.array([ # Observation covariance
+            [625, 0,  0, 0],
+            [0,   36, 0, 0],
+            [0,   0,  0, 0],
+            [0,   0,  0, 0]
+        ])
+        with numpy.errstate(divide='ignore', invalid='ignore'):
+            kalman_gain = predicted_covar / (predicted_covar + R) # Kalman gain represents confidence in measurement over prediction
+        kalman_gain = numpy.nan_to_num(kalman_gain)
+
+        print('Kalman gain:')
+        print(kalman_gain)
+
+
+        corrected_state = predicted_state + numpy.dot(kalman_gain, observation - predicted_state)
+        corrected_covar = numpy.dot(I - kalman_gain, predicted_covar)
+
+        print('Result:')
+        print(corrected_state)
+
+        self.last_state = corrected_state
+        self.last_covar = corrected_covar
+        return corrected_state
+
+
--- kdemo1.py
+++ kdemo1.py
@@ -4,27 +4,44 @@ import numpy
 
 from Kalman import Kalman
 
+# based on https://www.youtube.com/watch?v=fwM66qohrJ8
+
 initial_state = numpy.array([
-    [0], # x
-    [0], # x'
-    [20],# y
-    [0]  # y'
+    [4000], # x
+    [280], # x'
+    [3000],# y
+    [120]  # y'
 ])
 initial_covar = numpy.array([
-    [0],
-    [0],
-    [0],
-    [0]
+    [400, 0, 0, 0], # x
+    [0, 25, 0, 0], # x'
+    [0, 0, 0, 0], # y
+    [0, 0, 0, 0]  # y'
 ])
 
 acceleration = numpy.array([
-    [0],
-    [-9.8]
+    [2],
+    [0]
 ])
 
 dt = 1
 
 kfilter = Kalman(initial_state, initial_covar)
 
-for i in range(0,10):
-    print(kfilter.estimate(None, acceleration, dt))
+observation = numpy.array([
+    [4260], # x
+    [282],  # x'
+    [0],    # y
+    [0]     # y'
+])
+kfilter.estimate(observation, acceleration, dt)
+
+observation = numpy.array([
+    [4550], # x
+    [285],  # x'
+    [0],    # y
+    [0]     # y'
+])
+
+kfilter.estimate(observation, acceleration, dt)
+
