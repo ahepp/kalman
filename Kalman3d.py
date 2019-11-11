c37b2a9633c0df4d8a8dc543222a903adf229324
Andrew Hepp <drewhepp@umich.edu>
2019-11-10 21:30:22 -0500

    added 3d filter, fixed B matrix

--- Kalman3d.py
+++ Kalman3d.py
@@ -8,46 +8,52 @@ class Kalman:
     def estimate(self, measurement, acceleration, dt):
         # Predict the state and covariance
         A = numpy.array([
-            [1, dt, 0, 0 ], # x
-            [0,  1, 0, 0 ], # x'
-            [0,  0, 1, dt], # y
-            [0,  0, 0, 1 ]  # y'
+            [1, dt, 0, 0 , 0, 0 ], # x
+            [0,  1, 0, 0 , 0, 0 ], # x'
+            [0,  0, 1, dt, 0, 0 ], # y
+            [0,  0, 0, 1 , 0, 0 ]  # y'
+            [0,  0, 0, 0 , 1, dt], # z
+            [0,  0, 0, 0 , 0, 1 ]  # z'
         ])
         a = .5 * dt ** 2 # acceleration term
         B = numpy.array([
-            [a,  0 ], #x
-            [dt, 0 ], #x'
-            [0,  a ]  #y
-            [0,  dt], #y'
+            [a,  0 , 0 ], #x
+            [dt, 0 , 0 ], #x'
+            [0,  a , 0 ]  #y
+            [0,  dt, 0 ], #y'
+            [0,  0 , a ]  #z
+            [0,  0 , dt], #z'
         ])
         predicted_state = numpy.dot(A, self.last_state) + numpy.dot(B, acceleration) # + noise
         predicted_covar = numpy.dot(numpy.dot(A, self.last_covar), numpy.transpose(A)) # + noise
 
-        # Mask the covariance, since x and y are known to be independent
-        predicted_covar = numpy.diag(numpy.diag(predicted_covar))
-
-
-        # Extract position to incorporate into our model
+        # Extract information to incorporate into our model
         C = numpy.array([
-            [1, 0, 0, 0], # x
-            [0, 1, 0, 0], # x'
-            [0, 0, 0, 0], # y
-            [0, 0, 0, 0]  # y'
+            [1, 0 , 0, 0 , 0, 0 ], # x
+            [0,  1, 0, 0 , 0, 0 ], # x'
+            [0,  0, 1, 0 , 0, 0 ], # y
+            [0,  0, 0, 1 , 0, 0 ]  # y'
+            [0,  0, 0, 0 , 1, 0 ], # z
+            [0,  0, 0, 0 , 0, 1 ]  # z'
         ])
         observation = numpy.dot(C, measurement) # + noise
 
         # Calculate Kalman gain to correct our prediction based on our measurement
         I = numpy.array([
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
+            [1, 0 , 0, 0 , 0, 0 ], # x
+            [0,  1, 0, 0 , 0, 0 ], # x'
+            [0,  0, 1, 0 , 0, 0 ], # y
+            [0,  0, 0, 1 , 0, 0 ]  # y'
+            [0,  0, 0, 0 , 1, 0 ], # z
+            [0,  0, 0, 0 , 0, 1 ]  # z'
         ])
-        R = numpy.array([ # Observation covariance
-            [625, 0,  0, 0],
-            [0,   36, 0, 0],
-            [0,   0,  0, 0],
-            [0,   0,  0, 0]
+        R = numpy.array([
+            [1, 0 , 0, 0 , 0, 0 ], # x
+            [0,  1, 0, 0 , 0, 0 ], # x'
+            [0,  0, 1, 0 , 0, 0 ], # y
+            [0,  0, 0, 1 , 0, 0 ]  # y'
+            [0,  0, 0, 0 , 1, 0 ], # z
+            [0,  0, 0, 0 , 0, 1 ]  # z'
         ])
         with numpy.errstate(divide='ignore', invalid='ignore'):
             kalman_gain = predicted_covar / (predicted_covar + R) # Kalman gain represents confidence in measurement over prediction
